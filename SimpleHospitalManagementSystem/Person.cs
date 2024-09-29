6e807e2f48a858042bad3495b913a94c0af19010
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-29 15:28:58 +0400

    create Person Class ,then adding Attributes,Constructor, and displayInfo()Method

---
+++ SimpleHospitalManagementSystem/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Person
+    {
+        //Attributes
+        public enum Gender { Male, Female }
+        public string Name{get;private set;}
+        public int Age { get; private set; }
+        public Gender GenderType { get; private set; }
+
+        //Constructor: Initialize the attributes above.
+        public Person(string Name,int Age, Gender GenderType) 
+        { 
+            this.Name = Name;
+            this.Age = Age;
+            this.GenderType = GenderType;
+        }
+        //Method: DisplayInfo() to print the person’s details.
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name} \t age: {Age} \t Gender: {GenderType} ");
+        }
+
+
+
+    }
+}
