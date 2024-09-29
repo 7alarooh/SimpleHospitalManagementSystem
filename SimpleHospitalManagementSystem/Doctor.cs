fd384cbbe5974d27e3acb8b0c7758e0605fce50f
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-29 16:08:59 +0400

    create Doctor Class (Inherits from Person) , then adding attributes specific to doctor, and constructor to initialize the attributes

---
+++ SimpleHospitalManagementSystem/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Doctor:Person
+    {
+        //Attributes specific to doctor
+        public int DoctorID { get; set; }   
+        public string Specialization {  get; set; }
+        public List<Patient> PatientsList { get; set; }
+
+        //Constructor to initialize the attributes
+        public Doctor(string Name, int Age, Gender Gender, int DoctorID, string Specialization)
+            : base(Name, Age, Gender)
+        {
+            this.DoctorID = DoctorID;
+            this.Specialization = Specialization;
+            this.PatientsList = new List<Patient>();
+        }
+    }
+}
