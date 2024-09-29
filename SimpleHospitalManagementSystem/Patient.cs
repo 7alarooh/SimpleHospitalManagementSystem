32e875c1aa385bf5206e3f704f35cbf0f027347a
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-29 15:52:58 +0400

    Attributes specific to patient, and Constructor to initialize the attributes and assign a doctor in Patient class

---
+++ SimpleHospitalManagementSystem/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Patient:Person
+    {
+        //Attributes specific to patient
+        public int PatientID { get; set; }
+        public string Ailment { get; set; }
+        public Doctor AssignedDoctor { get; set; }
+        public Room room { get; set; }
+
+        //Constructor to initialize the attributes and assign a doctor
+        public Patient(string Name, int Age, Gender Gender, int PatientID, string Ailment, Doctor Dr, Room room)
+            : base(Name, Age, Gender) 
+        { 
+            this.PatientID = PatientID;
+            this.Ailment = Ailment;
+            AssignedDoctor = Dr;
+            room = null;
+        }
+
+
+
+
+    }
+}
