29725a20680d2c60f256aa3cf9dd9c892172c86e
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-30 01:51:50 +0400

    create  Appointment class  , adding attributes and  Constructor to initialize the attributes

---
+++ SimpleHospitalManagementSystem/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Appointment
+    {
+
+        //attributes
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime AppointmentDate { get;private set; }
+        public bool IsScheduled { get; set; }
+
+        //Constructor to initialize the attributes
+        public Appointment(Patient patient, Doctor Doctor) 
+        {
+            Patient = patient;
+            this.Doctor = Doctor;
+            IsScheduled = false;
+        }    
+
+    }
+}
