05b6516c1f76c2bf95e8df94470aabc0fca0234b
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-30 08:50:54 +0400

    create Hospital Class, and adding Attributes and  constructor initializes the lists

---
+++ SimpleHospitalManagementSystem/Hospital.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Hospital
+    {
+        //Attributes
+        public List<Doctor> DoctorsList { get; set; }
+        public List<Patient> PatientsList {  get; set; }
+        public List<Room> RoomsList { get; set; }
+
+        //Constructor initializes the lists
+        public Hospital() 
+        {
+            DoctorsList = new List<Doctor>();
+            PatientsList = new List<Patient>();
+            RoomsList = new List<Room>();
+        }
+
+    }
+}
