8e96c928f52c874574406eb66dd7ece0ac2396ff
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-01 12:30:35 +0400

    Method to add rooms to the clinic in Clinic class

--- SimpleHospitalManagementSystem/Clinic.cs
+++ SimpleHospitalManagementSystem/Clinic.cs
@@ -21,5 +21,30 @@ namespace SimpleHospitalManagementSystem
         public List<Room> Rooms { get; private set; }  // List of rooms in the clinic
         public Dictionary<Doctor, List<Appointment>> AvailableAppointments { get; private set; }  // Doctor-appointment mapping
 
+        // Constructor to initialize attributes
+        public Clinic(int ClinicID, string ClinicName, Specialization Specialization)
+        {
+            this.ClinicID = ClinicID;
+            this.ClinicName = ClinicName;
+            this.Specialization = Specialization;
+            Rooms = new List<Room>();
+            // Initialize the AvailableAppointments dictionary
+            AvailableAppointments = new Dictionary<Doctor, List<Appointment>>();
+        }
+
+        //Methods
+        // Method to add rooms to the clinic
+        public void AddRoom(Room room)
+        {
+            if (room == null)
+            {
+                Console.WriteLine("Invalid room.");
+                return;
+            }
+
+            Rooms.Add(room);
+            Console.WriteLine($"Room {room.RoomNumber} added to the clinic.");
+        }
+
     }
 }
