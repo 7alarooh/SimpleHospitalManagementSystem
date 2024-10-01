8bc5bc6b5cc27dc8ccb443f83f6122e10e24cb79
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-01 10:57:47 +0400

    create the InPatient (inherits from Patient) class and added Additional attributes specific and Constructor to initialize

---
+++ SimpleHospitalManagementSystem/InPatient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class InPatient:Patient
+    {
+        // Additional attributes specific to in-patient
+        public Room AssignedRoom { get; private set; }
+        public Doctor AssignedDoctor { get; private set; }
+        public DateTime AdmissionDate { get; private set; }
+        // Constructor to initialize the in-patient attributes
+        public InPatient(int patientID, string name, int age, Gender gender, string ailment, Doctor doctor, DateTime admissionDate)
+            : base(patientID, name, age, gender, ailment)
+        {
+            this.AssignedDoctor = doctor;
+            this.AdmissionDate = admissionDate;
+            this.AssignedRoom = null; // Initially no room assigned
+        }
+    }
+}
