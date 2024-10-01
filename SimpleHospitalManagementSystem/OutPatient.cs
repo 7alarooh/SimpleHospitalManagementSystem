3d5a7d33b7350ed088f4a4fe2bc1223cb7423ae3
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-01 11:50:46 +0400

    create OutPatient (inherits from Patient) class , and added Attribute specific and Constructor to initialize

---
+++ SimpleHospitalManagementSystem/OutPatient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class OutPatient : Patient
+    {
+        // Attribute specific 
+        public Clinic ClinicAssigned { get; private set; }
+
+        // Constructor to initialize the out-patient attributes
+        public OutPatient(int patientID, string name, int age, Gender gender, string ailment, Clinic clinicAssigned)
+            : base(patientID, name, age, gender, ailment)
+        {
+            this.ClinicAssigned = clinicAssigned;
+        }
+    }
+}
