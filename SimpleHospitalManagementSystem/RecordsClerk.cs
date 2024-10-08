6eee8193d7df007c5cc6b284fee35fae44d2f49d
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-08 09:40:06 +0400

    create RecordsClerk; and add attributes, and constructor for class

---
+++ SimpleHospitalManagementSystem/RecordsClerk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class RecordsClerk:Person
+    {
+        public int ClerkID { get; set; }
+
+        public RecordsClerk(int clerkID, string name, int age, Gender gender, string password)
+            : base(name, age, gender, password)
+        {
+            this.ClerkID = clerkID;
+        }
+
+    }
+}
